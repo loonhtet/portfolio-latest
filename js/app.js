224a081c4f546073fb71e201f40ad1247ea71cc2
loonhtet <gurdamanson@gmail.com>
2022-11-11 18:16:33 +0630

    11/11/22

--- js/app.js
+++ js/app.js
@@ -3,9 +3,16 @@ const topbar = document.querySelector(".top");
 const midbar = document.querySelector(".mid");
 const bottombar = document.querySelector(".bottom");
 
+hamburgerbtnhref = document.querySelectorAll("#abtn");
+// console.log(hamburgerbtnhref);
+
 hamburgerbtn.addEventListener("click", () => {
   hamburgerbtn.classList.toggle("active");
   topbar.classList.toggle("active");
   midbar.classList.toggle("active");
   bottombar.classList.toggle("active");
+
+  hamburgerbtnhref.forEach(function (e) {
+    e.classList.toggle("active");
+  });
 });
